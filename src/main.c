45ebc1abfd06954f1f01141e89d9365cd3408695
frank400 <j.victorhuguenin2018@gmail.com>
2020-12-04 19:53:12 -0300

    feat: adiciona funções para verificar um ganhador

--- src/main.c
+++ src/main.c
@@ -2,7 +2,90 @@
 #include <stdlib.h>
 
 #define DIM 3
-
+int diagonal1(int mat[DIM][DIM])
+{
+    int cont = 1;
+    for (int i = 0; i < DIM-1; i++)
+    {
+        if (coordenada(mat[i][i]) && mat[i][i] == mat[i + 1][i + 1])
+        {
+            cont++;
+        }
+    }
+    if (cont == DIM)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int diagonal2(int mat[DIM][DIM])
+{
+    int cont = 1;
+    for (int i = 0; i < DIM-1; i++)
+    {
+        if (coordenada(mat[i][DIM - i - 1]) && mat[i][DIM - i - 1] == mat[i + 1][DIM - i - 2])
+        {
+            cont++;
+        }
+    }
+    if (cont == DIM)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int coluna(int mat[DIM][DIM]) {
+    int cont = 1;
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM - 1; j++) {
+            if (coordenada(mat[j][i]) && mat[j][i] == mat[j + 1][i]) {
+                cont++;
+            }
+        }
+        if (cont == DIM) {
+            return 1;
+        }
+        cont = 1;
+    }
+    return 0;
+}
+int coordenada(int num)
+{
+    if (num == 1 || num == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int linhas (int mat[DIM][DIM])
+{
+    int cont = 1;
+    for (int i = 0; i < DIM; i++)
+    {
+        for (int j = 0; j < DIM-1; j++)
+        {
+            if (coordenada(mat[i][j]) && mat[i][j] == mat[i][j + 1])
+            {
+                cont++;
+            }
+        }
+        if (cont == DIM)
+        {
+            return 1;
+        }
+        cont = 1;
+    }
+    return 0;
+}
 int main()
 {
     int mat[DIM][DIM];
