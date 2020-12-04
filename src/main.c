8e3a015b7c23da422e6359ce66d56a9902f0abb0
lucasparente1998 <72233781+lucasparente1998@users.noreply.github.com>
2020-12-04 19:48:13 -0300

    feat: inicio estrutura basica jogo

--- src/main.c
+++ src/main.c
@@ -16,6 +16,29 @@ int main()
         }
     }
     printf("\n");
+
+
+    int x, y, CV = 0, jogos = 0, ganhar = 0, jogador = 1;
+
+    do{
+    
+        do{
+            if(jogador == 1){
+                printf("Primeiro jogador:\n");
+            }else {
+                printf("Segundo jogador:\n");
+            }
+            scanf("%d %d", &x, &y);
+            x = x - 1;
+            y = y - 1;
+            CV = coordenadaCerta(x, y);
+            if (CV == 1)
+            {
+                CV = CV + posicaVazia(x, y,mat);
+            }
+            
+        } while (CV != 2);
+    }while (ganhar == 0 && jogos < 9);
     
     return 0;
 }
